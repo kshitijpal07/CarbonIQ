ffc9991aaae0a5219926328a88dedacbf877c493
kshitijpal07 <kshitijpalforever@gmail.com>
2025-08-01 00:20:58 +0530

    server.py render update

--- backend/server.py
+++ backend/server.py
@@ -94,5 +94,7 @@ def handle_bill_upload():
             os.remove(pdf_path)
 
 if __name__ == '__main__':
-    # Runs the server on http://localhost:5001
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
+
